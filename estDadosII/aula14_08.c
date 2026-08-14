6abf32f5cd2d6dd00f3b6529f6ae3631baa49378
Geilson Lucas <geilson.gears@outlook.com>
2026-08-14 19:55:35 -0300

    Update aula14_08.c

--- estDadosII/aula14_08.c
+++ estDadosII/aula14_08.c
@@ -2,13 +2,14 @@
 #include <stdlib.h>
 
 typedef struct Aluno{
+    int id;
     char nome[50];
     float nota;
 }aluno;
 
 int main()
 {
-    FILE *arquivo = fopen("alunos.txt", "w");
+    FILE *arquivo = fopen("alunos.txt", "w+");
     aluno alunos[50];
     
     if (arquivo == NULL) {
@@ -21,7 +22,32 @@ int main()
         scanf("%s",&alunos[i].nome);
         printf("Digite a nota do aluno %d: ", i + 1);
         scanf("%f",&alunos[i].nota);
-        fprintf(arquivo, "Aluno: %s || Nota: %.2f \n",alunos[i].nome, alunos[i].nota);
+        alunos[i].id = i+1;
+        fprintf(arquivo, "ID: %d || Aluno: %s || Nota: %.2f \n",alunos[i].id, alunos[i].nome, alunos[i].nota);
+    }
+    
+    int idAluno = 0;
+    int idAux;
+    char nomeAux[50];
+    float notaAux;
+    
+    printf("Digite o id do aluno que quer encontrar: ");
+    scanf("%d",&idAluno);
+    
+    rewind(arquivo);
+    
+    for(int i = 0; i < 3; i++){
+        fscanf(arquivo, " ID: %d || Aluno: %s || Nota: %f",
+           &idAux,
+           &nomeAux,
+           &notaAux);
+
+        if(idAluno == idAux){
+            printf("ID: %d || Aluno: %s || Nota: %.2f\n",
+                   idAux,
+                   nomeAux,
+                   notaAux);
+        }
     }
     
     fclose(arquivo);
